fde172673380cb88931ce54259b297966fb29689
Екатерина Горяинова <kateas27@mail.ru>
2021-12-06 21:14:47 +0200

    Добавьте файлы проекта.

---
+++ lab14WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab14WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Хлеб",
+                ProductPrice = 65,
+                ProductImage = "Data\\hleb.png",
+                ProductCategory = ProductCategories.Eat
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Банан",
+                ProductPrice = 130,
+                ProductImage = "Data\\banan.png",
+                ProductCategory = ProductCategories.Eat
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Телевизор",
+                ProductPrice = 38000,
+                ProductImage = "Data\\tv.png",
+                ProductCategory = ProductCategories.Appliances
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ lab14WpfApp/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14WpfApp
+{
+    public enum ProductCategories
+    {
+        Eat,
+        Appliances
+    }
+    public class Product
+    {
+        public string ProductName {get; set;}
+        public double ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public ProductCategories ProductCategory { get; set; }
+    }
+}
